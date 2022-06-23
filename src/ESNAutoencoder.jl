7595de9701b57505665642361fa65f83132c338d
ngiann <ngiann@gmail.com>
2022-06-23 22:58:37 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.28

---
+++ src/ESNAutoencoder.jl
@@ -0,0 +1,5 @@
+module ESNAutoencoder
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ESNAutoencoder
+using Test
+
+@testset "ESNAutoencoder.jl" begin
+    # Write your tests here.
+end
